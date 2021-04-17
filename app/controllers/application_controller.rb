f0f4d2a9f94efad318d6c6458916c33d1a28be19
swareham-booth <swareham@chicagobooth.edu>
2021-04-17 02:20:17 +0000

    100%

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,4 +29,28 @@ class ApplicationController < ActionController::Base
     @square_root_of_num = @num.to_f ** 0.5
     render({:template => "calculation_templates/square_root_results.html.erb"})
   end
+
+  def blank_payment_form
+    render({ :template => "calculation_templates/payment_form.html.erb"})
+  end
+
+ 
+  def calculate_payment
+    apr = params.fetch("user_apr").to_f
+    years = params.fetch("user_years").to_f
+    principal = params.fetch("user_pv").to_f
+    period_rate = apr / 100.0 / 12
+    months = years * 12.0
+    monthly_payment = (period_rate * principal) / (1 - (1 + period_rate)**(months * -1))
+
+    @apr_text = apr.round(4).to_s + "%" #sprintf("%2.4f", apr) + "%"
+    #@apr_text = apr.to_s(:percentage)
+    @years_text = years.to_i
+    @principal_text = principal.to_s(:currency)
+    #@principal_text = sprintf("$%2.2f", principal)
+    @monthly_payment_text = monthly_payment.to_s(:currency)
+    
+
+    render({:template => "calculation_templates/payment_results.html.erb"})
+  end
 end
